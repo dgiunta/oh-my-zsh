7a2bc904f4738e9104627b1ba77d9f3a8ffd577f
Dave Giunta <dgiunta@mac.com>
2010-01-29 00:49:04 +0000

    Modifying the aliases for apache and nginx

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -4,17 +4,13 @@ alias la='ls -laH'
 alias rst="touch tmp/restart.txt"
 
 alias free="free -m"
+
 alias update="sudo aptitude update"
 alias install="sudo aptitude install"
 alias upgrade="sudo aptitude safe-upgrade"
 alias remove="sudo aptitude remove"
 alias search="sudo aptitude search"
-alias nginx-start="sudo /etc/init.d/nginx start"
-alias nginx-stop="sudo /etc/init.d/nginx stop"
-alias nginx-restart="sudo /etc/init.d/nginx restart"
 
-alias apache2-start="sudo /etc/init.d/apache2 start"
-alias apache2-stop="sudo /etc/init.d/apache2 stop"
-alias apache2-restart="sudo /etc/init.d/apache2 restart"
-alias apache2="sudo /etc/init.d/apache2"
+alias nginx="sudo /etc/init.d/nginx"
+alias apache="sudo /etc/init.d/apache2"
 
