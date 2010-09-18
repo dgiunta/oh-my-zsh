30eb28fd0fe6cdd7641bfe5b147ea3a3195f1235
Dave Giunta <dgiunta@mac.com>
2010-09-18 23:05:57 +0000

    adding enable / disable site commands

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -5,3 +5,11 @@ function h {
 function reload {
   source ~/.zshrc
 }
+
+function enable_site {
+  ln -s /home/deploy/sites-available/$1 /home/deploy/sites-enabled
+}
+
+function disable_site {
+  rm /home/deploy/sites-enabled/$1
+}
